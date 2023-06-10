fb10b4857056fe7f1f16a511b58a01ce9cbde5fb
Blindinlights <chenyutong007@gmail.com>
2023-06-10 17:47:27 +0800

    Stream output and example

---
+++ examples/stream/main.rs
@@ -0,0 +1,25 @@
+use edge_gpt::{ChatSession, ConversationStyle, StreamExt};
+use ezio::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let mut bot = ChatSession::create(ConversationStyle::Creative,&[])
+        .await
+        .unwrap();
+
+    loop {
+        println!("Ask the question please:");
+        let question = stdio::read_line();
+        println!("Waiting for bing for response ...");
+        let mut stream = bot.ask_stream(&question).await.unwrap();
+        let mut msg = String::new();
+        print!(">> ");
+        while let Some(Ok(response)) = stream.next().await {
+            if !response.text.is_empty() {
+                print!("{}", response.text.trim().trim_start_matches(&msg));
+                msg = response.text.trim().to_string();
+            }
+        }
+        println!();
+    }
+}
